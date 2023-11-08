2c8f25da07278dfbd09d60f905ef0523d35ba6b3
cruz <joaoflacruz@gmail.com>
2023-11-08 09:50:05 -0300

    Adicionando constantes e editando a Aposta

--- code/headers/models/aposta/Aposta.hpp
+++ code/headers/models/aposta/Aposta.hpp
@@ -2,12 +2,22 @@
 
 #include "Jogador.hpp"
 
+#define GANHO_GRUPO 18
+#define GANHO_DEZENA 60
+#define GANHO_CENTENA 600
+#define GANHO_MILHAR 4000
+#define GANHO_DUQUE_GRUPO 18.5
+#define GANHO_DUQUE_DEZENA 300
+#define GANHO_TERNO_GRUPO 130
+#define GANHO_TERNO_DEZENA 3000
+
 class Aposta{
 
     private:
         int valor_;
-        Jogador jogador_;
+        int ganho_;
+        bool cabeca_;
+        std::string nome_do_jogador_;
     public:
         Aposta();
-
 };
--- code/headers/models/aposta/duque/Duque.hpp
+++ code/headers/models/aposta/duque/Duque.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Aposta.hpp"
+#include "Grupo.hpp"
 
 class Duque : public Aposta {
 
