5a818085ac2d35ddd5bd88c5c43ff4d9555b3c8f
cruz <joaoflacruz@gmail.com>
2023-11-09 15:57:22 -0300

    Adicionando atributos e métodos à classe Duque

--- code/headers/models/aposta/duque/Duque.hpp
+++ code/headers/models/aposta/duque/Duque.hpp
@@ -8,10 +8,18 @@
 class Duque : public Aposta {
 
     private:
-
+        std::vector<Grupos> apostas_[2]; //Guarda as apostas do Duque
     public:
 
-        Duque();
-        ~Duque();
+        //CONSTRUTORES E DESTRUTORES
+        Duque(std::string nome_do_jogador, float valor, std::vector<Grupos> apostas, bool cabeca = false); //Construtor de Duque
+        ~Duque(); //Destrutor de Duque
 
+        //GETTERS
+        std::vector<Grupos> apostas(); //Getter para o atributo apostas
+
+        //MÉTODOS CONCRETOS
+
+        //MÉTODOS ABSTRATOS
+        virtual void verificar_vitoria(); //Verificação da vitória com atualização da variavel ganhou
 };
