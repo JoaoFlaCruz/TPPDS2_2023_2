3d76e3a17647c9ff8fdc990f7e7fe2de36a40a0f
mdfa99 <149254395+mdfa99@users.noreply.github.com>
2023-11-10 23:24:03 -0300

    Update Jogador.hpp

--- code/headers/models/Jogador.hpp
+++ code/headers/models/Jogador.hpp
@@ -1,16 +1,29 @@
 #pragma once
-
-#include <string>
-
+#include "Sorteio.hpp"
+#include "ListaSorteio.hpp"
 #include "Carteira.hpp"
 
-class Jogador{
-    private:
-        std::string apelido_;
-        Carteira carteira_;
-    public:
-        void recebe_dinheiro(float valor);
-        void retira_dinheiro(float valor);
-        std::string apelido();
-        Carteira carteira();
+class Jogador {
+public:
+	//Apostar em um sorteio diponivel
+	//Pré-condição: Sorteio tem que existir e estar diposnivel
+	void apostar(Sorteio& sorteio_diponivel);
+
+	//Retornar nome do jogador
+	std::string nome();
+
+	//Rerornar a carteira do jogador
+	Carteira carteira();
+
+	//Retornar a lista de sorteio participados
+	ListaSorteio sorteios_participados();
+
+	//Retornar CPF
+	std::string cpf();
+
+private:
+	std::string nome_;
+	std::string cpf_;
+	Carteira carteira_;
+	ListaSorteio sorteios_participados_;
 };
