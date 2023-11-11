c73bfa4b3d1012c9bf4eae3502526b58af5ea451
mdfa99 <149254395+mdfa99@users.noreply.github.com>
2023-11-10 23:21:47 -0300

    Update Carteira.hpp

--- code/headers/models/Carteira.hpp
+++ code/headers/models/Carteira.hpp
@@ -1,7 +1,23 @@
 #pragma once
+#include "Extrato.hpp"
+class Carteira {
+public:
+	//Retorna o saldo da carteira
+	float saldo();
 
-class Carteira{
-    private:
+	//Depositar
+	//Pré-condição: valor tem que ser valido
+	void depositar(float valor);
+	
+	//Receber valor da banca
+	//Pré-condição: valor tem que ser valido
+	void receber(float valor);
 
-    public:
+	//Pagar banca
+	//Pré-condição: valor tem que ser valido
+	void pagar(float valor);
+
+private:
+	float saldo_;
+	Extrato extrato_;
 };
