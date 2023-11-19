0da053e857c9b642b62159137b6efbb0355cdbc4
geanderson404 <129343629+geanderson404@users.noreply.github.com>
2023-11-19 19:57:16 -0300

    Update ListaUsuario.cpp

--- code/src/ListaUsuario.cpp
+++ code/src/ListaUsuario.cpp
@@ -1,1 +1,34 @@
 #include "ListaUsuario.hpp"
+
+ListaUsuario::ListaUsuario() {}
+
+// Adiciona um novo usuário ao vetor usuarios_
+void ListaUsuario::adicionarUsuario(const Usuario& usuario) {
+    // Verifica se o usuário já existe
+    if (validarLogin(usuario.obterNome())) {
+        throw "Usuário já existe na lista.";
+    }
+
+    usuarios_.push_back(usuario);
+}
+
+// Verifica se um determinado login existe no vetor usuarios_
+bool ListaUsuario::validarLogin(const std::string& login) const {
+    for (const auto& usuario : usuarios_) {
+        if (usuario.obterNome() == login) {
+            return true;  // Login encontrado
+        }
+    }
+    return false;  // Login não encontrado
+}
+
+// Retorna um objeto Usuario com o login especificado, se existir
+Usuario ListaUsuario::buscarUsuario(const std::string& login) const {
+    for (const auto& usuario : usuarios_) {
+        if (usuario.obterNome() == login) {
+            return usuario;  // Retorna o usuário encontrado por valor
+        }
+    }
+
+    throw "Usuário não encontrado na lista.";
+}
