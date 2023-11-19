5e82ba252188709c81226ea5365569a26690e3d2
geanderson404 <129343629+geanderson404@users.noreply.github.com>
2023-11-19 19:51:35 -0300

    Update Usuario.hpp

--- code/headers/models/Usuario.hpp
+++ code/headers/models/Usuario.hpp
@@ -1,26 +1,27 @@
-#pragma once
-
 #include <string>
-
 #include "Jogador.hpp"
 
-class Usuario{
-    private:
-        std::string nome_;
-        std::string senha_;
-        std::string cpf_;
-        Jogador jogador_;
-    public:
-        Usuario(
-            std::string nome,
-            std::string senha,
-            std::string cpf,
-            Jogador jogador);
-        ~Usuario();
+class Usuario {
+private:
+    std::string nome_;
+    std::string senha_;
+    std::string cpf_;
+    Jogador jogador_;
 
-        void alterar_senha(std::string senha_nova);
-        std::string nome();
-        std::string senha();
-        std::string cpf();
-        Jogador jogador();
+public:
+    
+    Usuario(std::string nome, std::string senha, std::string cpf, Jogador jogador);
+
+    
+    ~Usuario();
+
+    // Métodos de acesso (getter) para os membros privados
+    std::string obterNome() const;
+    std::string obterSenha() const;
+    std::string obterCPF() const;
+    Jogador obterJogador() const;
+
+    // Método para alterar a senha //opcional podem tirar se quiserem
+    void alterarSenha(std::string senhaNova);
 };
+
