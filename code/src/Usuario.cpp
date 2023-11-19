f2bda6728d0964039dce958a698f34b34def1d65
geanderson404 <129343629+geanderson404@users.noreply.github.com>
2023-11-19 18:48:51 -0300

    classe usuario.cpp

--- code/src/Usuario.cpp
+++ code/src/Usuario.cpp
@@ -1,1 +1,27 @@
 #include "Usuario.hpp"
+
+Usuario::Usuario(std::string nome, std::string senha, std::string cpf, Jogador jogador)
+    : nome_(nome), senha_(senha), cpf_(cpf), jogador_(jogador) {}
+
+Usuario::~Usuario() {}
+
+void Usuario::alterar_senha(std::string senha_nova) {
+    senha_ = senha_nova;
+}
+
+std::string Usuario::nome() {
+    return nome_;
+}
+
+std::string Usuario::senha() {
+    return senha_;
+}
+
+std::string Usuario::cpf() {
+    return cpf_;
+}
+
+// Método para obter o objeto Jogador associado ao usuário
+Jogador Usuario::jogador() {
+    return jogador_;
+}
