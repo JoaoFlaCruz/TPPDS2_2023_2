479a81095037e9adac442ce542a5af40cc128d92
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-21 03:32:13 -0300

    Atualizando ListaSorteios.hpp

--- code/headers/models/ListaSorteios.hpp
+++ code/headers/models/ListaSorteios.hpp
@@ -1,9 +1,14 @@
 #pragma once
 #include "Sorteio.hpp"
 #include <list>
+//Realizar os tratamentos de excessões
+//Testar
 
-class Lista_sorteio {
+class ListaSorteios {
 public:
+	//Inicializa uma lista de sorteios vazia
+	ListaSorteios();
+
 	//Retorna o tamanho da lista 
 	int tamanho();
 
@@ -17,6 +22,18 @@ public:
 	//Remove uma aposta na lista
 	//Pre-Condição: deve existir essa aposta
 	void remover_sorteio(Sorteio sorteio);
+
+	//Buscar sorteios em andamento 
+	ListaSorteios sorteios_em_andamento();
+
+	//Buscar sorteios feitos
+	ListaSorteios sorteios_feitos();
+
+	//Buscar sorteio por nome(Data e hora)
+	Sorteio buscar_sorteio_por_data_e_horario(std::string data_e_horario);
+
+	//Buscar participações de um jogador
+	ListaSorteios buscar_participacao(std::string nome);
 
 private:
 	std::list<Sorteio> lista_de_sorteio_;
