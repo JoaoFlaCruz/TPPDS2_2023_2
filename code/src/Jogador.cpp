b39c0387e584d6fcc1eafa5954caf93200963a97
mdfa99 <149254395+mdfa99@users.noreply.github.com>
2023-11-22 15:21:13 -0300

    Update Jogador.cpp

--- code/src/Jogador.cpp
+++ code/src/Jogador.cpp
@@ -1,1 +1,16 @@
 #include "Jogador.hpp"
+//Testar
+
+Jogador::Jogador(std::string apelido) : apelido_(apelido) {
+
+}
+
+std::string Jogador::apelido(){
+	return this->apelido_;
+}
+
+Carteira Jogador::carteira(){
+	return this->carteira_;
+}
+
+
