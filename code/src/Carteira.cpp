dc87e9b40a5a004de56ca7902a083a2acea208f5
mdfa99 <149254395+mdfa99@users.noreply.github.com>
2023-11-22 15:20:00 -0300

    Update Carteira.cpp

--- code/src/Carteira.cpp
+++ code/src/Carteira.cpp
@@ -1,1 +1,22 @@
 #include "Carteira.hpp"
+
+Carteira::Carteira() : saldo_(0), extrato_(Extrato()) {
+
+}
+
+float Carteira::saldo(){
+    return this->saldo_;
+}
+
+Extrato Carteira::extrato(){
+    return this->extrato_;
+}
+
+void Carteira::depositar(float valor){
+    if (valor <= 0) {
+        throw ValorInvalido{ valor };
+    }
+    this->saldo_ += valor;
+    this->extrato_.adicionar_movimentacao("Depositado", valor);
+}
+
