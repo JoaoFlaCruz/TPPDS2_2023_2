568e54f6f98723e0d9ae52666765e5fe55f93595
mdfa99 <149254395+mdfa99@users.noreply.github.com>
2023-11-22 15:17:58 -0300

    Update Extrato.hpp

--- code/headers/models/Extrato.hpp
+++ code/headers/models/Extrato.hpp
@@ -3,11 +3,18 @@
 #include <list>
 #include <string>
 #include <utility>
+//Pronto
+struct ExtratoVazio {
+};
 
 class Extrato {
+	
 public:
 	//Inicializa um extrato vazio
 	Extrato();
+
+	//Retorna o tamanho da lista
+	int tamanho();
 
 	//Retorna um extrato por padrão em ordem cronologica
 	//Pré-condição:tamanho do extrato deve ser maior que zero
@@ -20,9 +27,6 @@ public:
 	//Adiciona uma movimentação
 	//Pré-condição: o valor passado tem que ser valido
 	void adicionar_movimentacao(std::string descricao, float valor);
-
-	//Retorna o tamanho da lista
-	int tamanho();
 
 private:
 	//Lista de movimentações que guarda se recebido, depositado ou pago, data e valor 
