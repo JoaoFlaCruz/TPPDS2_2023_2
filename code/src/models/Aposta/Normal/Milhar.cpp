241f0c5099a0a863a8116af0afbba02435cbdd20
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 14:44:47 -0300

    Implementando Milhar.cpp

--- code/src/models/Aposta/Normal/Milhar.cpp
+++ code/src/models/Aposta/Normal/Milhar.cpp
@@ -1,1 +1,24 @@
 #include "Milhar.hpp"
+
+//Testar
+Milhar::Milhar(std::string nome_do_jogador, float valor, std::vector<int> apostas, bool cabeca = false)
+    : Normal(nome_do_jogador,valor, apostas, cabeca){}
+
+Milhar::~Milhar(){}
+
+void Milhar::verificar_vitoria(std::vector<int> resultado){
+
+    int primeira_aposta = apostas_[0];
+    bool se_aposta_foi_sorteada = false;
+    
+    for(int i = 0; i < 5; i++) {
+        if( primeira_aposta == resultado[i]) {
+            se_aposta_foi_sorteada = true;
+        }
+    }
+
+    if( se_aposta_foi_sorteada ) {
+        ganhou_ = true;
+    } else {
+        ganhou_ = false;
+    }
