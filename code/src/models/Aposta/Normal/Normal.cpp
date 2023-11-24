17113a00a330ded01d0fc314a63be37529ee2830
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 14:34:15 -0300

    Implementando Normal.cpp

--- code/src/models/Aposta/Normal/Normal.cpp
+++ code/src/models/Aposta/Normal/Normal.cpp
@@ -1,1 +1,24 @@
 #include "Normal.hpp"
+
+Normal::Normal(std::string nome_do_jogador, float valor, std::vector<int> apostas, bool cabeca = false)
+    : Aposta(nome_do_jogador, valor, cabeca){
+
+    if((apostas[0] < 0) ){
+        throw ValorDaApostaNegativo{};
+    }
+
+    if((apostas[0] > 99) ){
+        throw ValorDaApostaMuitoAlto{};
+    }
+
+    apostas_ = apostas;
+
+}
+
+Normal::~Normal() {}
+
+std::vector<int> Normal::apostas() {
+
+    return apostas_;
+
+}
