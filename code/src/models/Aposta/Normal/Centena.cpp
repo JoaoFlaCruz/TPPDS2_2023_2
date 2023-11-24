b9417f6abc20f9019279db93c23ed32caeec4081
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 14:41:30 -0300

    Implementando Centena.cpp

--- code/src/models/Aposta/Normal/Centena.cpp
+++ code/src/models/Aposta/Normal/Centena.cpp
@@ -1,1 +1,26 @@
 #include "Centena.hpp"
+
+//Testar
+Centena::Centena(std::string nome_do_jogador, float valor, std::vector<int> apostas, bool cabeca = false)
+    : Normal(nome_do_jogador,valor, apostas, cabeca){}
+
+Centena::~Centena(){}
+
+void Centena::verificar_vitoria(std::vector<int> resultado){
+
+    int primeira_aposta = apostas_[0];
+    bool se_aposta_foi_sorteada = false;
+    
+    for(int i = 0; i < 5; i++) {
+        if( primeira_aposta == (int)(resultado[i]%1000 / 4 + 1)) {
+            se_aposta_foi_sorteada = true;
+        }
+    }
+
+    if( se_aposta_foi_sorteada ) {
+        ganhou_ = true;
+    } else {
+        ganhou_ = false;
+    }
+
+}
