075b05c87ab0648d6e9455846fc00b72c11bee96
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 22:39:48 -0300

    Alterando DuqueGrupo.hpp

--- code/headers/models/aposta/duque/DuqueGrupo.hpp
+++ code/headers/models/aposta/duque/DuqueGrupo.hpp
@@ -7,10 +7,10 @@
 class DuqueGrupo : public Duque {
 
     private:
-        const static float ganho_ = GANHO_DUQUE_GRUPO; //Ganho de 18.5 para uma classe DuqueDezena
+        const float ganho_ = GANHO_DUQUE_GRUPO; //Ganho de 18.5 para uma classe DuqueDezena
     public:
         //CONSTRUTORES E DESTRUTORES
-        DuqueGrupo(std::string nome_do_jogador, float valor, std::vector<int> apostas, bool cabeca = false); //Construtor de DuqueGrupo
+        DuqueGrupo(std::string nome_do_jogador, float valor, bool cabeca, std::array<int, 2> apostas); //Construtor de DuqueGrupo
         ~DuqueGrupo(); //Destrutor de DuqueGrupo
 
         //GETTERS
@@ -18,5 +18,6 @@ class DuqueGrupo : public Duque {
         //MÉTODOS CONCRETOS
 
         //MÉTODOS ABSTRATOS
-        void verificar_vitoria(std::vector<int> resultado); //Verificação da vitória com atualização da variavel ganhou
+        void verificar_vitoria(std::array<int, 5> resultado); //Verificação da vitória com atualização da variavel ganhou
+        bool operator==(Aposta outra) override;
 };
