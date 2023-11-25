dcf3bb31037f2ce1ad44cb55071ee43d1a81d005
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 22:35:33 -0300

    Adicionando a declaração de um novo método em cCentena.hpp

--- code/headers/models/aposta/normal/Centena.hpp
+++ code/headers/models/aposta/normal/Centena.hpp
@@ -8,13 +8,15 @@ class Centena : public Normal {
 
     private:
 
-     const static float ganho_ = GANHO_CENTENA;
+     const float ganho_ = GANHO_CENTENA;
 
     public:
         /*Construtor*/
-        Centena(float valor, std::string nome_do_jogador, bool cabeca = false, std::vector<int> apostas);
+        Centena(float valor, std::string nome_do_jogador, bool cabeca, std::array<int, 1> apostas);
+
         /*Destrutor*/
         ~Centena();
 
-        void verificar_vitoria(std::vector<int> resultado); /*Verificação da vitória com atualização da variavel ganho*/
+        void verificar_vitoria(std::array<int, 5> resultado); /*Verificação da vitória com atualização da variavel ganho*/
+        bool operator==(Aposta outra) override;
 };
