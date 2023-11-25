52b78297046cb24654821846fd2af9a1c1803db1
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 23:12:55 -0300

    Endereçamento de headerfiles em Centena.cpp

--- code/src/models/Aposta/Normal/Centena.cpp
+++ code/src/models/Aposta/Normal/Centena.cpp
@@ -1,12 +1,13 @@
-#include "Centena.hpp"
+#include "../../headers/models/aposta/normal/Centena.hpp"
 
 //Testar
-Centena::Centena(std::string nome_do_jogador, float valor, std::vector<int> apostas, bool cabeca = false)
-    : Normal(nome_do_jogador,valor, apostas, cabeca){}
+Centena::Centena(float valor, std::string nome_do_jogador, bool cabeca, std::array<int, 1> apostas)
+    : Normal(nome_do_jogador, valor, cabeca, apostas) {}
+
 
 Centena::~Centena(){}
 
-void Centena::verificar_vitoria(std::vector<int> resultado){
+void Centena::verificar_vitoria(std::array<int, 5> resultado){
 
     int primeira_aposta = apostas_[0];
     bool se_aposta_foi_sorteada = false;
@@ -23,4 +24,14 @@ void Centena::verificar_vitoria(std::vector<int> resultado){
         ganhou_ = false;
     }
 
+}
+
+bool Centena::operator==(Aposta outra){
+    if (this->nome_do_jogador_ == outra.nome_do_jogador()
+        && this->valor_ == outra.valor() && this->cabeca_ == outra.cabeca()
+        && this->ganhou() == outra.ganhou() && this->apostas_ == outra.apostas()) {
+        return 1;
+     }
+   
+    return false;
 }
