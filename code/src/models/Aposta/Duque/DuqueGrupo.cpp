f5e889eacd0821efd3bc14bcd0850cd121351e58
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 23:17:46 -0300

    Alterando a nomenclatura de algumas variáveis para aumentar a legibilidade do codigo emDuqueGrupo.cpp

--- code/src/models/Aposta/Duque/DuqueGrupo.cpp
+++ code/src/models/Aposta/Duque/DuqueGrupo.cpp
@@ -1,31 +1,40 @@
-#include "DuqueGrupo.hpp"
+#include "../../headers/models/aposta/duque/DuqueGrupo.hpp"
 
-DuqueGrupo::DuqueGrupo(std::string nome_do_jogador, float valor, std::vector<int> apostas, bool cabeca = false)
-    : Duque(nome_do_jogador,valor, apostas, cabeca){}
+DuqueGrupo::DuqueGrupo(std::string nome_do_jogador, float valor, bool cabeca, std::array<int, 2> apostas)
+    : Duque(nome_do_jogador, valor, cabeca, apostas) {}
 
 DuqueGrupo::~DuqueGrupo(){}
 
-void DuqueGrupo::verificar_vitoria(std::vector<int> resultado){
+void DuqueGrupo::verificar_vitoria(std::array<int, 5> resultado){
 
-    int aposta_a = apostas_[0];
-    bool verificador_a = false;
-    int aposta_b = apostas_[1];
-    bool verificador_b = false;
+    int primeira_aposta = apostas_[0];
+    bool verificador_primeira_aposta = false;
+    int segunda_aposta = apostas_[1];
+    bool verificador_segunda_aposta = false;
 
     for(int i = 0; i < 5; i++) {
         int grupo = ((int) (resultado[i]%100)/25) + 1;
-        if( grupo == aposta_a) {
-            verificador_a = true;
+        if( grupo == primeira_aposta) {
+            verificador_primeira_aposta = true;
         }
-        if( grupo == aposta_b) {
-            verificador_b = true;
+        if( grupo == segunda_aposta) {
+            verificador_segunda_aposta = true;
         }
     }
 
-    if( verificador_a && verificador_b) {
+    if(verificador_primeira_aposta && verificador_segunda_aposta) {
         ganhou_ = true;
     } else {
         ganhou_ = false;
     }
 
+}
+bool DuqueGrupo::operator==(Aposta outra) {
+    if (this->nome_do_jogador_ == outra.nome_do_jogador()
+        && this->valor_ == outra.valor() && this->cabeca_ == outra.cabeca()
+        && this->ganhou() == outra.ganhou() && this->apostas_ == outra.apostas()) {
+        return 1;
+    }
+
+    return false;
 }
