f1dbbe9a31716c89b22bcf68e626d28a148c50ac
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 23:10:56 -0300

    Manutenção e corrigindo endereçamento Duque.cpp

--- code/src/models/Aposta/Duque/Duque.cpp
+++ code/src/models/Aposta/Duque/Duque.cpp
@@ -1,29 +1,36 @@
-#include "Duque.hpp"
+#include "../../headers/models/aposta/duque/Duque.hpp"
 
-Duque::Duque(std::string nome_do_jogador, float valor, std::vector<int> apostas, bool cabeca = false)
+Duque::Duque(std::string nome_do_jogador, float valor, bool cabeca, std::array<int, 2> apostas)
     : Aposta(nome_do_jogador, valor, cabeca){
 
 
     if(apostas[0] == apostas[1]){
-        throw ValorDasApostasIgual{};
+        throw NumerosDasApostasIguais{ apostas[0], apostas[1]};
     }
 
-    if((apostas[0] < 0) || (apostas[1] < 0)){
-        throw ValorDaApostaNegativo{};
+    if((apostas[0] < 0)){
+        throw NumeroDaApostaNegativo{ apostas[0] };
+    }
+    if (apostas[1] < 0) {
+        throw NumeroDaApostaNegativo{ apostas[1] };
     }
 
-    if((apostas[0] > 99) || (apostas[1] > 99)){
-        throw ValorDaApostaMuitoAlto{};
+    if((apostas[0] > 99)){
+        throw NumeroDaApostaMuitoAlto{ apostas[0] };
+    }
+    if ((apostas[1] > 99)) {
+        throw NumeroDaApostaMuitoAlto{ apostas[1]};
     }
 
-    apostas_ = apostas;
+    for (int i = 0; i < 2; i++) {
+        this->apostas_.push_back(apostas[i]);
+    }
 
 }
 
 Duque::~Duque() {}
 
 std::vector<int> Duque::apostas() {
-
     return apostas_;
 
 }
