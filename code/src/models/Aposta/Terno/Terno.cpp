afba0527a5f28e2533919a66953b334f1e82eafb
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 23:11:27 -0300

    Manutenção e endereçamento de Terno.cpp

--- code/src/models/Aposta/Terno/Terno.cpp
+++ code/src/models/Aposta/Terno/Terno.cpp
@@ -1,22 +1,44 @@
-#include "Terno.hpp"
+#include "../../headers/models/aposta/terno/Terno.hpp"
 
-Terno::Terno(std::string nome_do_jogador, float valor, std::vector<int> apostas, bool cabeca = false)
+Terno::Terno(std::string nome_do_jogador, float valor, bool cabeca, std::array<int, 3> apostas)
     : Aposta(nome_do_jogador, valor, cabeca){
 
 
-    if((apostas[0] == apostas[1]) || (apostas[0] == apostas[2]) || (apostas[1] == apostas[2])){
-        throw ValorDasApostasIgual{};
+    if( apostas[0] == apostas[1] ){
+        throw NumerosDasApostasIguais{apostas[0], apostas[1]};
+    }
+    if (apostas[0] == apostas[2]) {
+        throw NumerosDasApostasIguais{ apostas[0], apostas[2] };
+    }
+    if ( apostas[1] == apostas[2] ) {
+        throw NumerosDasApostasIguais{ apostas[1], apostas[2] };
     }
 
-    if((apostas[0] < 0) || (apostas[1] < 0) || (apostas[3] < 0)){
-        throw ValorDaApostaNegativo{};
+    if(apostas[0] < 0){
+        throw NumeroDaApostaNegativo{ apostas[0] };
+    }
+    if (apostas[1] < 0) {
+        throw NumeroDaApostaNegativo{ apostas[1] };
+    }
+    if (apostas[2] < 0) {
+        throw NumeroDaApostaNegativo{ apostas[2] };
     }
 
-    if((apostas[0] > 99) || (apostas[1] > 99) || (apostas[2] > 99)){
-        throw ValorDaApostaMuitoAlto{};
+
+    if(apostas[0] > 99){
+        throw NumeroDaApostaMuitoAlto{ apostas[0] };
+    }
+    if (apostas[1] > 99) {
+        throw NumeroDaApostaMuitoAlto{ apostas[1] };
+    }
+    if (apostas[2] > 99) {
+        throw NumeroDaApostaMuitoAlto{ apostas[2] };
     }
 
-    apostas_ = apostas;
+    for (int i = 0; i < 3; i++) {
+        this->apostas_.push_back(apostas[i]);
+    }
+
 
 }
 
