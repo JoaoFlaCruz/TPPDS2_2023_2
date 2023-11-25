091912e12bae7b9caa6f6d2af28d2c241ca8dbcb
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 23:10:03 -0300

    Corrigindo e manutenção de Normal.cpp

--- code/src/models/Aposta/Normal/Normal.cpp
+++ code/src/models/Aposta/Normal/Normal.cpp
@@ -1,24 +1,22 @@
-#include "Normal.hpp"
+#include "../../headers/models/aposta/normal/Normal.hpp"
 
-Normal::Normal(std::string nome_do_jogador, float valor, std::vector<int> apostas, bool cabeca = false)
-    : Aposta(nome_do_jogador, valor, cabeca){
-
-    if((apostas[0] < 0) ){
-        throw ValorDaApostaNegativo{};
+Normal::Normal(std::string nome_do_jogador, float valor, bool cabeca, std::array<int, 1> apostas) : Aposta(nome_do_jogador, valor, cabeca) {
+    if ((this->apostas_[0] < 0)) {
+        throw NumeroDaApostaNegativo{ apostas[0] };
     }
 
-    if((apostas[0] > 99) ){
-        throw ValorDaApostaMuitoAlto{};
+    if ((apostas[0] > 99)) {
+        throw NumeroDaApostaMuitoAlto{ apostas[0] };
     }
 
-    apostas_ = apostas;
-
+    for (int i = 0; i < 1; i++) {
+        this->apostas_.push_back(apostas[i]);
+    }
 }
 
 Normal::~Normal() {}
 
 std::vector<int> Normal::apostas() {
-
     return apostas_;
 
 }
