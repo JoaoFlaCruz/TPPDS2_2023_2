e1a64dcb16b8cd1c61c22679e2f5ecbe80d594cf
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 23:09:11 -0300

    Manutenção em Aposta.cpp

--- code/src/models/Aposta/Aposta.cpp
+++ code/src/models/Aposta/Aposta.cpp
@@ -1,12 +1,24 @@
-#include "Aposta.hpp"
-#include <stdexcept>
+#include "../../headers/models/aposta/Aposta.hpp"
 #include <iostream>
+
+void Aposta::verificar_vitoria(std::array<int, 5> resultado) {
+}
+
+bool Aposta::operator==(Aposta outra){
+    if (this->nome_do_jogador_ == outra.nome_do_jogador_ && this->valor_ == outra.valor_ && this->cabeca_ == outra.cabeca_ && this->ganhou_ == outra.ganhou_) {
+        return 1;
+    }
+    return false;
+}
 
 // CONSTRUTORES E DESTRUTORES
 Aposta::Aposta(std::string nome_do_jogador, float valor, bool cabeca) : valor_(valor), cabeca_(cabeca), nome_do_jogador_(nome_do_jogador) {
     if (valor < 0) {
-        throw ValorDaApostaNegativo{"O valor da aposta não pode ser negativo."};
+        throw ValorDaApostaNegativo{ valor };
     }
+    this->nome_do_jogador_ = nome_do_jogador;
+    this->valor_ = valor;
+    this->cabeca_ = cabeca;
 }
 
 Aposta::~Aposta() {
@@ -34,15 +46,12 @@ bool Aposta::ganhou() {
     return ganhou_;
 }
 
+std::vector<int> Aposta::apostas(){
+
+    return this->apostas_;
+}
+
 // MÉTODOS CONCRETOS
 float Aposta::calcular_ganho() {
-    try {
-        if (valor_ < 0) {
-            throw std::logic_error("O valor da aposta não pode ser negativo.");
-        }
-        return valor_ * ganho_;
-    } catch (const std::exception& e) {
-        std::cerr << "Erro durante o cálculo do ganho: " << e.what() << std::endl;
-        return 0.0;
-    }
+   return valor_ * ganho_;
 }
