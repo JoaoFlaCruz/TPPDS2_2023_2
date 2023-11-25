8b1d05354d3dad923fd30fd75a5912c3c8236d3f
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 22:39:16 -0300

    Alterando DuqueDezena.hpp

--- code/headers/models/aposta/duque/DuqueDezena.hpp
+++ code/headers/models/aposta/duque/DuqueDezena.hpp
@@ -7,11 +7,11 @@
 class DuqueDezena : public Duque {
 
     private:
-        const static float ganho_ = GANHO_DUQUE_DEZENA; //Ganho de 300 para uma classe DuqueDezena
+        const float ganho_ = GANHO_DUQUE_DEZENA; //Ganho de 300 para uma classe DuqueDezena
     public:
 
         //CONSTRUTORES E DESTRUTORES
-        DuqueDezena(std::string nome_do_jogador, float valor, std::vector<int> apostas, bool cabeca = false); //Construtor de DuqueDezena
+        DuqueDezena(std::string nome_do_jogador, float valor, bool cabeca, std::array<int, 2> apostas); //Construtor de DuqueDezena
         ~DuqueDezena(); //Destrutor de DuqueDezena
 
         //GETTERS
@@ -19,5 +19,6 @@ class DuqueDezena : public Duque {
         //MÉTODOS CONCRETOS
 
         //MÉTODOS ABSTRATOS
-        void verificar_vitoria(std::vector<int> resultado); //Verificação da vitória com atualização da variavel ganhou
+        void verificar_vitoria(std::array<int, 5> resultado); //Verificação da vitória com atualização da variavel ganhou
+        bool operator==(Aposta outra) override;
 };
