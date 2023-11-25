8b55188d361e286ff21d54ebcbcd208f93f329ae
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 23:12:28 -0300

    Adicionando implementação de igualdade Dezena.cpp

--- code/src/models/Aposta/Normal/Dezena.cpp
+++ code/src/models/Aposta/Normal/Dezena.cpp
@@ -1,11 +1,13 @@
-#include "Dezena.hpp"
+#include "../../headers/models/aposta/normal/Dezena.hpp"
 //Testar
-Dezena::Dezena(std::string nome_do_jogador, float valor, std::vector<int> apostas, bool cabeca = false)
-    : Normal(nome_do_jogador,valor, apostas, cabeca){}
+
+Dezena::Dezena(std::string nome_do_jogador, float valor, bool cabeca, std::array<int, 1> apostas) : Normal(nome_do_jogador, valor, cabeca, apostas) {
+    
+}
 
 Dezena::~Dezena(){}
 
-void Dezena::verificar_vitoria(std::vector<int> resultado){
+void Dezena::verificar_vitoria(std::array<int, 5> resultado){
 
     int primeira_aposta = apostas_[0];
     bool se_aposta_foi_sorteada = false;
@@ -22,4 +24,13 @@ void Dezena::verificar_vitoria(std::vector<int> resultado){
         ganhou_ = false;
     }
 
+}
+bool Dezena::operator==(Aposta outra) {
+    if (this->nome_do_jogador_ == outra.nome_do_jogador()
+        && this->valor_ == outra.valor() && this->cabeca_ == outra.cabeca()
+        && this->ganhou() == outra.ganhou() && this->apostas_ == outra.apostas()) {
+        return 1;
+    }
+
+    return false;
 }
