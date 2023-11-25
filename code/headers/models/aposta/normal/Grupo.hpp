916802f0980a6650fa9f99e42e36faeb61e0e3c8
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-11-24 22:37:40 -0300

    Adicionado nova declaração Grupo.hpp

--- code/headers/models/aposta/normal/Grupo.hpp
+++ code/headers/models/aposta/normal/Grupo.hpp
@@ -10,9 +10,11 @@ class Grupo : public Normal {
 
     public:
         /*Construtor*/
-        Grupo(float valor, std::string nome_do_jogador, bool cabeca = false, std::vector<int> apostas);
+        Grupo(float valor, std::string nome_do_jogador, bool cabeca, std::array<int, 1> apostas);
+
         /*Destrutor*/
         ~Grupo();
 
-        void verificar_vitoria(std::vector<int> resultado); /*Verificação da vitória com atualização da variavel ganho*/
+        void verificar_vitoria(std::array<int, 5> resultado); /*Verificação da vitória com atualização da variavel ganho*/
+        bool operator==(Aposta outra) override;
 };
