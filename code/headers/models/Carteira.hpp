463abf31e4fc387d37fa569473008faa2be9721c
geanderson404 <129343629+geanderson404@users.noreply.github.com>
2023-11-27 14:01:43 -0300

    Update Carteira.hpp

--- code/headers/models/Carteira.hpp
+++ code/headers/models/Carteira.hpp
@@ -7,6 +7,12 @@ struct ValorInvalido {
 	float valor;
 };
 
+struct SaldoInsuficiente {
+    float valor;
+
+    SaldoInsuficiente(float valor) : valor(valor) {}
+};
+
 class Carteira {
 public:
 	//Inicializa uma carteira vazia 
@@ -21,10 +27,16 @@ public:
 	//Depositar
 	//Pré-condição: valor tem que ser maior que zero e positivo
 	void depositar(float valor);
+
+	//Sacar
+	//Pré-condição: valor tem que ser maior que zero e menor que o saldo
+	void sacar(float valor);
 	
 private:
 	float saldo_;
 	Extrato extrato_;
 };
+
+
 
 
