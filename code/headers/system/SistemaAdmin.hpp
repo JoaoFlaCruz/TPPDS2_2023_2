80a949727f230adafee818db0cc1bd9b3f42327a
geanderson404 <129343629+geanderson404@users.noreply.github.com>
2023-11-27 12:22:25 -0300

    Update SistemaAdmin.hpp

---
+++ code/headers/system/SistemaAdmin.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "../models/ListaSorteios.hpp"
+#include "../models/Sorteio.hpp"
+#include "../models/ListaApostas.hpp"
+#include "../models/aposta/Aposta.hpp"
+#include "../models/Jogador.hpp"
+#include "../models/Carteira.hpp"
+
+class SistemaAdmin {
+private:
+    ListaSorteios listaSorteios;
+
+public:
+    // ... (construtor, destrutor, etc.)
+
+    // Gerenciar sorteios
+    void criarSorteio(Sorteio &sorteio);
+    void sortear();
+    Jogador verificarGanhador();
+
+    // Realizar pagamento para os ganhadores
+    void realizarPagamento(Jogador &ganhador);
+
+    // Manipular a seção de sorteios
+    void adicionarSorteio(Sorteio &sorteio);
+    void removerSorteio(Sorteio &sorteio);
+    void exibirDetalhesSorteio(Sorteio &sorteio);
+
+    // Manipular a seção de apostas
+    void adicionarAposta(Aposta &aposta);
+    void removerAposta(Aposta &aposta);
+    void verificarApostas();
+
+    // Manipular a seção de jogadores
+    void adicionarJogador(Jogador &jogador);
+    void removerJogador(Jogador &jogador);
+    void exibirDetalhesJogador(Jogador &jogador);
+
+    // Manipular a seção de carteiras
+    void adicionarDinheiro(Carteira &carteira, double valor);
+    void retirarDinheiro(Carteira &carteira, double valor);
+    void exibirSaldo(Carteira &carteira);
+};
