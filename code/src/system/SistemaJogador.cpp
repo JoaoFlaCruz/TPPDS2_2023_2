739675bfd9bbea0113960fd2b0a3d45bf3b72194
geanderson404 <129343629+geanderson404@users.noreply.github.com>
2023-12-03 14:13:07 -0300

    Update SistemaJogador.cpp

--- code/src/system/SistemaJogador.cpp
+++ code/src/system/SistemaJogador.cpp
@@ -1,45 +1,54 @@
-#pragma once
+#include "./../../headers/system/SistemaJogador.hpp"
+#include <iostream>
+SistemaJogador::SistemaJogador(ListaUsuario& listaUsuarios, ListaApostas& listaApostas) 
+    : listaUsuarios_(listaUsuarios), listaApostas_(listaApostas), login_() {}
 
-#include "../models/Jogador.hpp"
-#include "../models/Carteira.hpp"
-#include "../models/ListaUsuario.hpp"
-#include "../models/aposta/Aposta.hpp"
-#include "../../headers/models/aposta/duque/DuqueDezena.hpp"
-#include "../models/Login.hpp"
+// Método para efetuar o login
+void SistemaJogador::login(const std::string& nome, const std::string& senha) {
+    Usuario usuario = listaUsuarios_.buscarUsuario(nome);
 
-struct LoginInvalido{
+    // Verifica se o jogador existe
+    if (usuario.nome() == "") {
+        throw LoginInvalido{};
+    }
 
-std::string mensagem;
-};
+    // Verifica se a senha está correta
+    if (usuario.senha() != senha) {
+        throw LoginInvalido{};
+    }
 
-struct ApostaInvalida{
+    // Define o usuário logado
+    login_.definirUsuarioLogado(usuario);
+}
+// Método para efetuar o logout
+void SistemaJogador::logout() {
+    login_.desconectarUsuario();
+}
 
-std::string mensagem;
-};
+// Método para realizar uma aposta
+void SistemaJogador::realizarAposta(float valor, const std::array<int, 2>& apostas) {
+    if (!login_.haUsuarioLogado()) {
+        throw ApostaInvalida{};
+    }
 
+    Jogador jogador = login_.obterUsuarioLogado().jogador();
 
-struct ExtratoInvalido{
+    if (jogador.carteira().saldo() < valor) {
+        throw ApostaInvalida{};
+    }
 
-std::string mensagem;
-};
-class SistemaJogador {
-public:
-    SistemaJogador(ListaUsuario& listaUsuarios, ListaApostas& listaApostas);
+    DuqueDezena aposta(login_.obterUsuarioLogado().nome(), valor, true, apostas);
+    jogador.carteira().sacar(valor);
+    listaApostas_.adicionar_aposta(aposta);
+}
+// Método para exibir o extrato do jogador
+void SistemaJogador::exibirExtrato() {
+    if (!login_.haUsuarioLogado()) {
+        throw ExtratoInvalido{};
+    }
 
-    // Método para efetuar o login
-    void login(const std::string& nome, const std::string& senha);
-
-    // Método para efetuar o logout
-    void logout();
-
-    // Método para realizar uma aposta
-    void realizarAposta(float valor, const std::array<int, 2>& apostas);
-
-    // Método para exibir o extrato do jogador
-    void exibirExtrato();
-
-private:
-    ListaUsuario& listaUsuarios_;
-    ListaApostas& listaApostas_;
-    Login login_;
-};
+    Jogador jogador = login_.obterUsuarioLogado().jogador();
+    Extrato extrato = jogador.carteira().extrato();
+    std::cout << "Extrato do jogador " << login_.obterUsuarioLogado().nome() << ":\n";
+    extrato.exibirMovimentacoes();
+}
