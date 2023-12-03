ae0bb32c34111f1bcb5666fdb40db55b969d19fa
geanderson404 <129343629+geanderson404@users.noreply.github.com>
2023-12-03 14:12:20 -0300

    Update SistemaJogador.cpp

--- code/src/system/SistemaJogador.cpp
+++ code/src/system/SistemaJogador.cpp
@@ -1,12 +1,45 @@
-#include "./../../headers/system/SistemaJogador.hpp"
+#pragma once
 
-#include <iostream>
+#include "../models/Jogador.hpp"
+#include "../models/Carteira.hpp"
+#include "../models/ListaUsuario.hpp"
+#include "../models/aposta/Aposta.hpp"
+#include "../../headers/models/aposta/duque/DuqueDezena.hpp"
+#include "../models/Login.hpp"
+
+struct LoginInvalido{
+
+std::string mensagem;
+};
+
+struct ApostaInvalida{
+
+std::string mensagem;
+};
 
 
-// Construtor da classe SistemaJogador
-SistemaJogador::SistemaJogador() {
-}
+struct ExtratoInvalido{
 
-// Destrutor da classe SistemaJogador
-SistemaJogador::~SistemaJogador() {
-}
+std::string mensagem;
+};
+class SistemaJogador {
+public:
+    SistemaJogador(ListaUsuario& listaUsuarios, ListaApostas& listaApostas);
+
+    // Método para efetuar o login
+    void login(const std::string& nome, const std::string& senha);
+
+    // Método para efetuar o logout
+    void logout();
+
+    // Método para realizar uma aposta
+    void realizarAposta(float valor, const std::array<int, 2>& apostas);
+
+    // Método para exibir o extrato do jogador
+    void exibirExtrato();
+
+private:
+    ListaUsuario& listaUsuarios_;
+    ListaApostas& listaApostas_;
+    Login login_;
+};
