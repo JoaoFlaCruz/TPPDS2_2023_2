4a83c2ba51236817040f601d447483472fb52490
geanderson404 <129343629+geanderson404@users.noreply.github.com>
2023-12-04 10:05:22 -0300

    Update Extrato.cpp

--- code/src/models/Extrato.cpp
+++ code/src/models/Extrato.cpp
@@ -1,20 +1,15 @@
+
 #include "../../headers/models/Extrato.hpp"
 #include <iostream>
-Extrato::Extrato() : extrato_({}), tamanho_(0) {}
 
-std::list<std::pair<std::string, float>> Extrato::extrato_por_ordem_cronologica() {
-    if (this->tamanho_ == 0) {
-        throw ExtratoVazio{};
-    }
-    return this->extrato_;
-}
+Extrato::Extrato() : extrato_({}), tamanho_(0) {}
 
 std::list<std::pair<std::string, float>> Extrato::ordenar_por_ordem_de_valor() {
     if (this->tamanho_ == 0) {
         throw ExtratoVazio{};
     }
     std::list<std::pair<std::string, float>> lista_reordenada = this->extrato_;
-    auto comparador = [](const auto& a, const auto& b) {
+    auto comparador = [](const std::pair<std::string, float>& a, const std::pair<std::string, float>& b) {
         return a.second > b.second;
     };
     lista_reordenada.sort(comparador);
