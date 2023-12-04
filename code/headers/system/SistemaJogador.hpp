b73743f7846653861e1ab3d7e22b3542e71249cc
geanderson404 <129343629+geanderson404@users.noreply.github.com>
2023-12-04 08:38:45 -0300

    Update SistemaJogador.hpp

--- code/headers/system/SistemaJogador.hpp
+++ code/headers/system/SistemaJogador.hpp
@@ -3,9 +3,18 @@
 #include "../models/Jogador.hpp"
 #include "../models/Carteira.hpp"
 #include "../models/ListaUsuario.hpp"
+#include "../models/ListaSorteios.hpp"
 #include "../models/aposta/Aposta.hpp"
 #include "../../headers/models/aposta/duque/DuqueDezena.hpp"
 #include "../models/Login.hpp"
+
+struct OperacaoInvalida{
+
+std::string mensagem;
+};
+
+
+
 
 struct LoginInvalido{
 
@@ -22,24 +31,27 @@ struct ExtratoInvalido{
 
 std::string mensagem;
 };
+
 class SistemaJogador {
 public:
-    SistemaJogador(ListaUsuario& listaUsuarios, ListaApostas& listaApostas);
+    SistemaJogador(ListaApostas& listaApostas, ListaUsuario& listaUsuarios);
 
-    // Método para efetuar o login
+    const Login& obterLogin() const;
+    const ListaUsuario& obterListaUsuarios() const;
+    const ListaApostas& obterListaApostas() const;
+
     void login(const std::string& nome, const std::string& senha);
-
-    // Método para efetuar o logout
     void logout();
-
-    // Método para realizar uma aposta
     void realizarAposta(float valor, const std::array<int, 2>& apostas);
-
-    // Método para exibir o extrato do jogador
     void exibirExtrato();
 
+    void adicionar_dinheiro(double valor);
+    void retirar_dinheiro(double valor);
+    void realizar_pagamento(Jogador& jogador, double valor);
+    float retornar_saldo();
+
 private:
-    ListaUsuario& listaUsuarios_;
-    ListaApostas& listaApostas_;
     Login login_;
+    ListaApostas& listaApostas_;
+    ListaUsuario& listaUsuarios_;
 };
