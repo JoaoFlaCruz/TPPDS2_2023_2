b43ea1792b2c1aa63f89abb7a2d7cb35340cc308
geanderson404 <129343629+geanderson404@users.noreply.github.com>
2023-12-04 10:00:41 -0300

    Update Interface.hpp

--- code/headers/interface/Interface.hpp
+++ code/headers/interface/Interface.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <stdlib.h>
 #include <iostream>
 #include <string>
@@ -17,49 +16,58 @@
 #define E4_PAG_CADASTRAR_SORTEIO 4
 #define E5_PAG_LISTAR_SORTEIO 5
 #define E6_PAG_CARTEIRA_ADMIN 6
+#define E2_PAG_JOGADOR 7
 
 //Excecoes lançadas
 struct EntradaInvalida {};
 struct SenhaInvalida {};
 
-class Interface{
-    private:
-        int estado_;
-        std::string msg_erro_;
-        bool erro_tratado_;
+class Interface {
+private:
+    int estado_;
+    std::string msg_erro_;
+    bool erro_tratado_;
 
-        SistemaAdmin sis_admin_;
-        SistemaGeral sis_geral_;
-        SistemaJogador sis_jogador_;
+    SistemaAdmin sis_admin_;
+    SistemaGeral sis_geral_;
+    SistemaJogador sis_jogador_;
 
-    public:
-        Interface();
-        ~Interface();
+public:
+    Interface();
+    ~Interface();
 
-        // Comandos básicos de interface gráfica
-        void limpar_tela();
-        void cabecalho();
-        void barra_final();
-        void quebra_linha();
-        void mensagem_de_erro();
-        int entrada_comando();
+    // Comandos básicos de interface gráfica
+    void limpar_tela();
+    void cabecalho();
+    void barra_final();
+    void quebra_linha();
+    void mensagem_de_erro();
+    int entrada_comando();
 
-        // Execução principal
-        void executar();
+    // Execução principal
+    void executar();
 
-        // Estados
-        // E0_PAG_INICIAL
-        void pagina_0_inicial();
-        // E1_PAG_LOGIN_ADMIN
-        void pagina_1_login_admin();
-        // E2_PAG_ADIM
-        void pagina_2_admin();
-        // E3_PAG_SORTEIO
-        void pagina_3_sorteio();
-        // E4_PAG_CADASTRAR_SORTEIO
-        void pag_4_cadastrar_sorteio();
-        // E5_PAG_LISTAR_SORTEIO
-        void pag_5_listar_sorteio();
-        // E6_PAG_CARTEIRA_ADMIN
-        void pag_6_carteira_admin();
+    // Estados
+    // E0_PAG_INICIAL
+    void pagina_0_inicial();
+    // E1_PAG_LOGIN_ADMIN
+    void pagina_1_login_admin();
+    // E2_PAG_ADMIN
+    void pagina_2_admin();
+    // E3_PAG_SORTEIO
+    void pagina_3_sorteio();
+    // E4_PAG_CADASTRAR_SORTEIO
+    void pag_4_cadastrar_sorteio();
+    // E5_PAG_LISTAR_SORTEIO
+    void pag_5_listar_sorteio();
+    // E6_PAG_CARTEIRA_ADMIN
+    void pag_6_carteira_admin();
+
+    // Estados relacionados ao SistemaJogador
+    // E7_PAG_CARTEIRA_JOGADOR
+    void pag_7_carteira_jogador();
+    // E8_PAG_JOGADOR
+    void pagina_8_jogador();
 };
+
+// INTERFACE_HPP
