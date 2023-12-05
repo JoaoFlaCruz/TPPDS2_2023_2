2d66d0e9f8907865eb0df470339abde46c33ad52
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-12-05 04:40:01 -0300

    Update Extrato.cpp

--- Versao_de_Seguranca_Rascunho/src/Extrato.cpp
+++ Versao_de_Seguranca_Rascunho/src/Extrato.cpp
@@ -1,42 +1,34 @@
+
 #include "../../headers/models/Extrato.hpp"
-Extrato::Extrato() : extrato_({}), tamanho_(0) {
+#include <iostream>
 
-}
+Extrato::Extrato() : extrato_({}), tamanho_(0) {}
 
-std::list<std::pair<std::string, float>> Extrato::extrato()
-{
-    return this->extrato_;
-}
-
-Extrato Extrato::extrato_por_ordem_cronologica() {
-    if (this->tamanho_ == 0) {
-        throw ExtratoVazio{};
-    }
-    Extrato extrato;
-    extrato.extrato_ = this->extrato_;
-    return extrato;
-}
-
-Extrato Extrato::ordenar_por_ordem_de_valor(){
+std::list<std::pair<std::string, float>> Extrato::ordenar_por_ordem_de_valor() {
     if (this->tamanho_ == 0) {
         throw ExtratoVazio{};
     }
     std::list<std::pair<std::string, float>> lista_reordenada = this->extrato_;
-    auto comparador = [](const auto& a, const auto& b) {
+    auto comparador = [](const std::pair<std::string, float>& a, const std::pair<std::string, float>& b) {
         return a.second > b.second;
     };
     lista_reordenada.sort(comparador);
 
-    Extrato extrato;
-    extrato.extrato_ = lista_reordenada;
-    return extrato;
+    return lista_reordenada;
 }
 
-
-void Extrato::adicionar_movimentacao(std::string descricao, float valor){
+void Extrato::adicionar_movimentacao(std::string descricao, float valor) {
     this->extrato_.push_back({descricao, valor});
     this->tamanho_++;
 }
+
 int Extrato::tamanho() {
     return this->tamanho_;
 }
+
+std::list<std::pair<std::string, float>> Extrato::extrato_por_ordem_cronologica()
+{
+    return this->extrato_;
+}
+
+
