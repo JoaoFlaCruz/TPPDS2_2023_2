ae532701a4fa2a2877ed84bc91062d7a947919aa
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-12-05 22:50:26 -0300

    Update Carteira.hpp

--- code/headers/models/Carteira.hpp
+++ code/headers/models/Carteira.hpp
@@ -1,71 +1,46 @@
-/**
- * @file Carteira.hpp
- * @brief Definição da classe Carteira.
- * @version 1.0
- * @date 2023-12-05
- */
-
 #pragma once
 #include "Extrato.hpp"
 
-/**
- * @brief Exceção lançada quando um valor é considerado inválido para operações na carteira.
- */
+//Pronta
+
 struct ValorInvalido {
-	float valor; /**< Valor inválido. */
+	float valor;
 };
 
-/**
- * @brief Exceção lançada quando uma tentativa de saque é maior que o saldo disponível na carteira.
- */
 struct SaldoInsuficiente {
-    float valor; /**< Valor do saque que resultou em saldo insuficiente. */
+    float valor;
 
-    /**
-     * @brief Construtor que inicializa a exceção com o valor do saque.
-     * @param valor Valor do saque.
-     */
     SaldoInsuficiente(float valor) : valor(valor) {}
 };
 
-/**
- * @brief Classe que representa a carteira do jogador.
- */
 class Carteira {
 public:
-	/**
-	 * @brief Construtor padrão da Carteira.
-	 */
+	//Inicializa uma carteira vazia 
 	Carteira();
 
-	/**
-	 * @brief Retorna o saldo atual da carteira.
-	 * @return Saldo da carteira.
-	 */
+	//Retorna o saldo da carteira
 	float saldo();
 
-	/**
-	 * @brief Retorna o extrato da carteira.
-	 * @return Extrato da carteira.
-	 */
+	//Retorna o extrato da carteira
 	Extrato extrato();
+
+	//Adiciona uma movimentação
+	//Pré-condição: o valor passado tem que ser valido
+	void adicionar_movimentacao(std::string descricao, float valor);
 	
-	/**
-	 * @brief Deposita um valor na carteira.
-	 * @param valor Valor a ser depositado.
-	 * @throw ValorInvalido se o valor não for maior que zero.
-	 */
+	//Depositar
+	//Pré-condição: valor tem que ser maior que zero e positivo
 	void depositar(float valor);
 
-	/**
-	 * @brief Saca um valor da carteira.
-	 * @param valor Valor a ser sacado.
-	 * @throw ValorInvalido se o valor não for maior que zero.
-	 * @throw SaldoInsuficiente se o valor do saque for maior que o saldo disponível na carteira.
-	 */
+	//Sacar
+	//Pré-condição: valor tem que ser maior que zero e menor que o saldo
 	void sacar(float valor);
 	
 private:
-	float saldo_; /**< Saldo atual da carteira. */
-	Extrato extrato_; /**< Extrato da carteira. */
+	float saldo_;
+	Extrato extrato_;
 };
+
+
+
+
