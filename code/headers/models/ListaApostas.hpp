3f8c34277f0ab6492fae8e5f846efa878b68a3fc
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-12-05 22:36:12 -0300

    Update ListaApostas.hpp

--- code/headers/models/ListaApostas.hpp
+++ code/headers/models/ListaApostas.hpp
@@ -1,84 +1,45 @@
-/**
- * @file ListaApostas.hpp
- * @brief Definição da classe ListaApostas.
- * @version 1.0
- * @date 2023-12-05
- */
-
+#pragma once
 #pragma once
 
 #include "aposta/Aposta.hpp"
 #include <list>
-
-/**
- * @brief Estrutura para representar uma lista de apostas vazia.
- */
-struct ListaDeApostaVazia {};
-
-/**
- * @brief Estrutura para representar uma aposta inexistente na lista.
- */
-struct ApostaInexistente {
-    Aposta aposta_que_nao_existe; ///< Aposta inexistente, não existe construtor para esse tipo de aposta.
+//Testar 
+struct ListaDeApostaVazia {
 };
 
-/**
- * @brief Classe que representa uma lista de apostas.
- */
+struct ApostaInexistente {
+	Aposta aposta_que_nao_existe;//Não existe construtor para esse tipo de aposta
+};
+
 class ListaApostas {
 public:
-    /**
-     * @brief Construtor da classe ListaApostas.
-     * Cria uma lista de apostas vazia.
-     */
-    ListaApostas();
+	//Cria uma lista de apostas vazia
+	ListaApostas();
 
-    /**
-     * @brief Retorna a lista de apostas.
-     * @return A lista de apostas.
-     */
-    std::list<Aposta> lista_de_aposta();
+	//Retorna a lista de apostas
+	std::list<Aposta> lista_de_aposta();
 
-    /**
-     * @brief Retorna o tamanho da lista de apostas.
-     * @return O tamanho da lista de apostas.
-     */
-    int tamanho();
+	//Retorna o tamanho da lista 
+	int tamanho();
 
-    /**
-     * @brief Adiciona uma nova aposta à lista.
-     * @param aposta A aposta a ser adicionada.
-     */
-    void adicionar_aposta(Aposta aposta);
+	//Adiciona uma nova aposta na lista
+	void adicionar_aposta(Aposta* aposta);//
 
-    /**
-     * @brief Remove uma aposta da lista.
-     * @param aposta A aposta a ser removida.
-     * @pre Deve existir a aposta a ser removida.
-     * @pre O tamanho da lista deve ser maior que zero.
-     */
-    void remover_aposta(Aposta aposta);
+	//Remove uma aposta na lista
+	//Pre-Condição: deve existir a aposta a ser removida
+	//Pre-condicao: o tamanho da lista tem que ser maior que zero 
+	void remover_aposta(Aposta aposta);//
+	
+	//Buscar aposta que ganharam
+	ListaApostas apostas_ganhas();
 
-    /**
-     * @brief Busca e retorna uma lista de apostas que foram ganhas.
-     * @return A lista de apostas ganhas.
-     */
-    ListaApostas apostas_ganhas();
+	//Libera todo espaço reservado para os conteudos da lista
+	~ListaApostas();
 
-    /**
-     * @brief Destrutor da classe ListaApostas.
-     * Libera todo o espaço reservado para os conteúdos da lista.
-     */
-    ~ListaApostas();
+	void verificar_apostas_ganhas(std::array<int, 5> resultados);
 
 private:
-    std::list<Aposta> lista_de_aposta_; ///< Lista de apostas.
-    int tamanho_; ///< Tamanho da lista de apostas.
-
-    /**
-     * @brief Verifica se uma aposta pertence à lista.
-     * @param aposta A aposta a ser verificada.
-     * @return True se a aposta pertence à lista, false caso contrário.
-     */
-    bool pertence(Aposta aposta);
+	std::list<Aposta*> lista_de_aposta_;
+	int tamanho_;
+	bool pertence(Aposta aposta);
 };
