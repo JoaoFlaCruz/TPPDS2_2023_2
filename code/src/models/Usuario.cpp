b6c32cef3d1b584e2b75257c1ead8f4084ca59ca
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-12-05 22:48:49 -0300

    Update Usuario.cpp

--- code/src/models/Usuario.cpp
+++ code/src/models/Usuario.cpp
@@ -28,19 +28,29 @@ Jogador Usuario::jogador() const {
     return jogador_;
 }
 
+float Usuario::saldo_da_carteira(){
+    return this->jogador_.saldo_da_carteira();
+}
+
+void Usuario::depositar_na_carteira(float valor_a_ser_depositado){
+    this->jogador_.depositar_na_carteira(valor_a_ser_depositado);
+}
+
+void Usuario::sacar_na_carteira(float valor_a_ser_sacado){
+    this->jogador_.sacar_na_carteira(valor_a_ser_sacado);
+}
+
+Extrato Usuario::extrato(){
+
+    return this->jogador_.extrato();
+}
+
+void Usuario::adicionar_movimentacao(std::string descricao, float valor){
+    this->jogador_.adicionar_movimentacao(descricao, valor);
+}
+
 // Método para alterar a senha
 void Usuario::alterarSenha(std::string senhaNova) {
     senha_ = senhaNova;
 }
 
-void Usuario::adicionar_fundos(float valor) {
-    jogador_.adicionar_fundos(valor);
-}
-
-void Usuario::retirar_fundos(float valor) {
-    jogador_.retirar_fundos(valor);
-}
-
-float Usuario::saldo() {
-    return jogador_.saldo();
-}
