04ca0863a5b547c33b7d891c710be0313bc0cdf9
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-12-05 22:50:42 -0300

    Update Extrato.hpp

--- code/headers/models/Extrato.hpp
+++ code/headers/models/Extrato.hpp
@@ -1,65 +1,46 @@
-/**
- * @file Extrato.hpp
- * @brief Definição da classe Extrato.
- * @version 1.0
- * @date 2023-12-05
- */
-
 #pragma once
+#include "Extrato.hpp"
 
-#include <list>
-#include <string>
-#include <utility>
+//Pronta
 
-/**
- * @brief Estrutura para representar um extrato vazio.
- */
-struct ExtratoVazio {};
-
-/**
- * @brief Classe que representa um extrato de movimentações financeiras.
- */
-class Extrato {
-public:
-    /**
-     * @brief Construtor da classe Extrato.
-     */
-    Extrato();
-
-    /**
-     * @brief Retorna o tamanho do extrato.
-     * @return O tamanho do extrato.
-     */
-    int tamanho();
-
-    /**
-     * @brief Retorna um extrato em ordem cronológica.
-     * @return Lista de pares representando o extrato em ordem cronológica.
-     * @pre O tamanho do extrato deve ser maior que zero.
-     */
-    std::list<std::pair<std::string, float>> extrato_por_ordem_cronologica();
-
-    /**
-     * @brief Retorna o extrato ordenado por ordem de valor.
-     * @return Lista de pares representando o extrato ordenado por ordem de valor.
-     * @pre O tamanho do extrato deve ser maior que zero.
-     */
-    std::list<std::pair<std::string, float>> ordenar_por_ordem_de_valor();
-
-    /**
-     * @brief Adiciona uma movimentação ao extrato.
-     * @param descricao Descrição da movimentação.
-     * @param valor Valor da movimentação.
-     * @pre O valor passado deve ser válido.
-     */
-    void adicionar_movimentacao(std::string descricao, float valor);
-
-    /**
-     * @brief Exibe as movimentações do extrato.
-     */
-    void exibirMovimentacoes() const;
-
-private:
-    std::list<std::pair<std::string, float>> extrato_; ///< Lista de movimentações que guarda descrição e valor.
-    int tamanho_; ///< Tamanho do extrato.
+struct ValorInvalido {
+	float valor;
 };
+
+struct SaldoInsuficiente {
+    float valor;
+
+    SaldoInsuficiente(float valor) : valor(valor) {}
+};
+
+class Carteira {
+public:
+	//Inicializa uma carteira vazia 
+	Carteira();
+
+	//Retorna o saldo da carteira
+	float saldo();
+
+	//Retorna o extrato da carteira
+	Extrato extrato();
+
+	//Adiciona uma movimentação
+	//Pré-condição: o valor passado tem que ser valido
+	void adicionar_movimentacao(std::string descricao, float valor);
+	
+	//Depositar
+	//Pré-condição: valor tem que ser maior que zero e positivo
+	void depositar(float valor);
+
+	//Sacar
+	//Pré-condição: valor tem que ser maior que zero e menor que o saldo
+	void sacar(float valor);
+	
+private:
+	float saldo_;
+	Extrato extrato_;
+};
+
+
+
+
