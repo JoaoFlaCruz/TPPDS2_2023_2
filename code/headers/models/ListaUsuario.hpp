03dcba11665dbb155db27aaf48141368ac9a8941
MayconOliv1 <129776451+MayconOliv1@users.noreply.github.com>
2023-12-05 22:38:43 -0300

    Update ListaUsuario.hpp

--- code/headers/models/ListaUsuario.hpp
+++ code/headers/models/ListaUsuario.hpp
@@ -1,66 +1,44 @@
-/**
- * @file ListaUsuario.hpp
- * @brief Definição da classe ListaUsuario.
- * @version 1.0
- * @date 2023-12-05
- */
-
 #pragma once
 
 #include <vector>
 #include "Usuario.hpp"
 
-/**
- * @brief Estrutura para representar uma exceção de usuário já existente.
- */
-struct UsuarioJaExiste {};
+struct LoginInvalido {
+    std::string mensagem;
+};
 
-/**
- * @brief Estrutura para representar uma exceção de usuário não encontrado.
- */
+struct UsuarioJaExiste {};
 struct UsuarioNaoEncontrado {};
 
-/**
- * @brief Classe que representa uma lista de usuários.
- */
 class ListaUsuario {
 public:
-    /**
-     * @brief Construtor da classe ListaUsuario.
-     * Cria uma lista de usuários vazia.
-     */
     ListaUsuario();
 
-    /**
-     * @brief Adiciona um novo usuário ao vetor de usuários.
-     * @param usuario O usuário a ser adicionado.
-     * @pre O usuário não deve existir na lista.
-     */
-    void adicionarUsuario(const Usuario& usuario);
+    //Retornar usuario logado
+    Usuario* usuario_logado();
 
-    /**
-     * @brief Verifica se um determinado login existe na lista de usuários.
-     * @param login O login a ser verificado.
-     * @return True se o login existir, false caso contrário.
-     * @pre A lista de usuários não deve estar vazia.
-     */
-    bool validarLogin(const std::string& login) const;
+    // Adiciona um novo usuário ao vetor usuarios_
+    // pré-condição: o usuário não deve existir na lista.
+    void adicionarUsuario(std::string nome, std::string senha, std::string cpf);
 
-    /**
-     * @brief Retorna um objeto Usuario com o login especificado, se existir.
-     * @param login O login do usuário a ser buscado.
-     * @return Referência para o objeto Usuario com o login especificado.
-     * @pre A lista de usuários não deve estar vazia.
-     */
-    Usuario& buscarUsuario(const std::string& login);
+    // Verifica se um determinado login existe no vetor usuarios_
+     // pré-condição: a lista de usuários não deve estar vazia
+    bool pertence(std::string nome);
 
-    /**
-     * @brief Remove um usuário com o login especificado da lista.
-     * @param login O login do usuário a ser removido.
-     * @pre A lista de usuários não deve estar vazia.
-     */
+    // Retorna um objeto Usuario com o login especificado, se existir
+     // pré-condição: a lista de usuários não deve estar vazia.
+    Usuario* buscarUsuario(const std::string& login);
+
     void removerUsuario(const std::string& login);
 
+    void login(std::string nome_do_usuario, std::string senha);
+
+    void logout();
+
 private:
-    std::vector<Usuario> usuarios_; ///< Vetor de usuários.
+    std::vector<Usuario> usuarios_;
+    Usuario* usuario_logado_;
 };
+
+
+
